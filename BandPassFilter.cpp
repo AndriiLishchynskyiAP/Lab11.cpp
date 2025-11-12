9d6435ca226f0195afdb71489172bb0f3e968ceb
Андрій <andrii.lishchynskyi.ap.2024@lpnu.ua>
2025-11-12 12:18:55 +0200

    Update BandPassFilter.cpp

--- BandPassFilter.cpp
+++ BandPassFilter.cpp
@@ -6,6 +6,5 @@ BandPassFilter::BandPassFilter(string n, double f, double r, double c, double bw
     : Filter(n, f, r, c), bandwidth(bw) {}
 
 double BandPassFilter::calculateImpedance(double frequency) const {
-    FilterData d = getData();
-    return d.resistance * sqrt(1 + pow((frequency - d.cutoffFrequency) / (bandwidth / 2), 2));
+    return data.resistance * sqrt(1 + pow((frequency - data.cutoffFrequency) / (bandwidth / 2), 2));
 }
