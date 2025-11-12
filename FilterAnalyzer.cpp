361b5fec8b5a373b0b0265092f011614376f415c
Андрій <andrii.lishchynskyi.ap.2024@lpnu.ua>
2025-11-12 12:20:18 +0200

    Update FilterAnalyzer.cpp

--- FilterAnalyzer.cpp
+++ FilterAnalyzer.cpp
@@ -5,7 +5,10 @@ using namespace std;
 void FilterAnalyzer::analyzeFilters(const vector<Filter*>& filters, double testFrequency) {
     cout << "\n=== АНАЛІЗ ФІЛЬТРІВ ===\n";
     for (auto f : filters) {
-        f->showInfo();
+        cout << "Назва фільтра: " << f->data.name << endl;
+        cout << "Частота зрізу: " << f->data.cutoffFrequency << " Гц" << endl;
+        cout << "Опір: " << f->data.resistance << " Ом" << endl;
+        cout << "Ємність: " << f->data.capacitance << " Ф" << endl;
         cout << "Імпеданс при " << testFrequency << " Гц: "
              << f->calculateImpedance(testFrequency) << " Ом\n\n";
     }
